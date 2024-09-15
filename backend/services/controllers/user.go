f4df3693ecb93dc736bf1183fc8cacad834a6a1b
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-09-15 14:31:25 -0300

    Add update method

--- backend/services/controllers/user.go
+++ backend/services/controllers/user.go
@@ -7,6 +7,7 @@ import (
 	"main.go/models"
 	"main.go/utils"
 	"net/http"
+	"strconv"
 )
 
 type UserController struct {
@@ -35,7 +36,27 @@ func (u UserController) Create(ctx echo.Context) error {
 	return utils.HTTPCreated(ctx, data)
 }
 
-func (u UserController) Update(ctx echo.Context) error  { panic("implement me") }
+func (u UserController) Update(ctx echo.Context) error {
+	idStr := ctx.Param("id")
+
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return utils.HTTPFail(ctx, http.StatusBadRequest, err, "id should be a number")
+	}
+
+	var data models.User
+	if err := ctx.Bind(&data); err != nil {
+		return utils.HTTPFail(ctx, http.StatusBadRequest, err, "failed to parse body")
+	}
+
+	if err := u.validator.Struct(&data); err != nil {
+		return utils.HTTPFail(ctx, http.StatusBadRequest, err, "failed to validate body")
+	}
+
+	// todo -> finish this code ( implement DB )
+	return utils.HTTPSucess(ctx, data)
+
+}
 func (u UserController) Delete(ctx echo.Context) error  { panic("implement me") }
 func (u UserController) GetList(ctx echo.Context) error { panic("implement me") }
 func (u UserController) Get(ctx echo.Context) error     { panic("implement me") }
